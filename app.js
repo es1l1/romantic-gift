ba9b9cba07f9d0bf7a09782f6b174373d682448b
es1l1 <242797738+es1l1@users.noreply.github.com>
2026-07-17 16:13:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
+let points = localStorage.getItem("lovePoints") || 0;
 
+
+function addPoints(number){
+
+points = Number(points)+number;
+
+localStorage.setItem(
+"lovePoints",
+points
+);
+
+updatePoints();
+
+}
+
+
+
+function updatePoints(){
+
+let box=document.getElementById("points");
+
+if(box){
+
+box.innerHTML=
+"❤️ نقاط الحب: "+points;
+
+}
+
+}
+
+
+
+window.onload=function(){
+
+updatePoints();
+
+}
